b4bcf6be123beafd6bd585b36ad44a03369f0f5a
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-07-31 11:46:16 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -4,20 +4,56 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 import google.generativeai as genai
+import cv2
+from deepface import DeepFace as df
+import time
+from fastapi.responses import StreamingResponse
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 app = FastAPI()
 
 GOOGLE_API_KEY = "AIzaSyCly-89E1Cr6N9jy88_PWQjuMJsaxD66cA"
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+def detect_face(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        try:
+            analyze = df.analyze(frame, actions = ['emotion'])
+            cv2.putText(frame, analyze[0]['dominant_emotion'],  (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        except:
+            cv2.putText(frame, "No face detected", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    return frame
+
+def gen_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            frame_with_faces = detect_face(frame)
+            _, buffer = cv2.imencode('.jpg', frame_with_faces)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.03)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "output_text": ""})
+
+
+@app.get('/video_feed')
+def video_feed():
+    return StreamingResponse(gen_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.post("/upload", response_class=HTMLResponse)
 async def generate_story(request: Request, topic: str = Form(...), genre: str = Form(...), word_limit: int = Form(...)):
