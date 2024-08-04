a01ae4c7cd7d7544c61ea45d5c74d728ecfcb0c0
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-08-04 09:07:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import google.generativeai as genai
 from deepface import DeepFace as df
 
-# Initialize Generative AI model
-GOOGLE_API_KEY = ""
+GOOGLE_API_KEY = st.secrets['API_KEY']
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
@@ -25,16 +24,14 @@ def detect_face(frame):
         except:
             pass
         
-        cv2.putText(frame, f"Emotion: {dominant_emotion}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     
     return frame, dominant_emotion
 
-# Streamlit UI
-st.title("Real-Time Face Detection")
-
-if st.button("Open Camera"):
+st.markdown("<h1 style='text-align: center;'>Storytellercv</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Enter word limit</p>", unsafe_allow_html=True)
+word_limit = st.number_input("", min_value=30, value=100)
+if st.button("Start"):
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    
     if not cap.isOpened():
         st.write("Error: Could not open camera.")
     else:
@@ -46,7 +43,7 @@ if st.button("Open Camera"):
             st.write("Emotion: ", dominant_emotion)
             
             if dominant_emotion != "No face detected":
-                response = model.generate_content(f"Write a story within 100 words. The story should improve my emotion: {dominant_emotion}. For example, if I'm angry, it should make me happy.")
+                response = model.generate_content(f"Write a story within {word_limit} words. The story should be Focused on emotional wellbeing and support. My emotion: {dominant_emotion}. For example, if I'm angry, it should make me happy.")
                 output_text = response.text
                 st.write("Story: ", output_text)
         
